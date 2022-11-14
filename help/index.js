f7d021174608c3a5f2da36115d3636ddd2812c1f
akrit-ghimire <akrit.ghimire.developer@gmail.com>
2022-11-14 17:41:00 +0000

    New Website to help Website Club

---
+++ help/index.js
@@ -0,0 +1,124 @@
+const menu_element = document.getElementById('menu')
+const page_element = document.getElementById('page')
+const forward_btn = document.getElementById('forward')
+const back_btn = document.getElementById('back')
+const page_info = document.getElementById('page_info')
+
+const page_viewer = {
+    markdown_parser: (text) => {
+        const toHTML = text
+        
+        .replace(/>/gim, '&gt;')
+        .replace(/</gim, '&lt;')
+        .replace(/^@@page-final/gim, '</div>') // final so no next button shows
+        .replace(/^@@page-end/gim, '<button class="next" onclick="page_changer.move_slide(1)">next page <i class="material-icons">arrow_forward</i></button></div>') // end page
+        .replace(/^@@page-start/gim, '<div data-page>') // start page
+
+        .replace(/^#code-end#/gim, '</pre></code>') // end code
+        .replace(/^#code-start#/gim, '<code><pre>') // start code
+
+        .replace(/^#a#(.*$)/gim, '<a href="$1" target="_blank">$1</a>') // link
+        .replace(/^- (.*$)/gim, '<li>$1</li>') // list
+        .replace(/^#newline/gim, '<br>') // new line
+        
+        .replace(/^!(.*$)/gim, '<img src="$1">') // h3 tag
+        .replace(/^### (.*$)/gim, '<h3>$1</h3>') // h3 tag
+        .replace(/^## (.*$)/gim, '<h2>$1</h2>') // h2 tag
+        .replace(/^# (.*$)/gim, '<h1>$1</h1>') // h1 tag
+        .replace(/\*\*(.*)\*\*/gim, '<b>$1</b>') // bold text
+        .replace(/\*(.*)\*/gim, '<i>$1</i>'); // italic text
+
+        return toHTML.trim(); // using trim method to remove whitespace
+        // code originally by Randolph Perkins - Medium Article
+    },
+    display_markdown: (markdown_data) => {
+        const markdown = document.querySelector('md')
+        markdown.innerHTML = page_viewer.markdown_parser(markdown_data)
+    },
+    read_file: (file) => {
+        const f = new XMLHttpRequest()
+        f.open("GET", file, false)
+        f.onreadystatechange = () => {
+            if (f.readyState === 4 && (f.status === 200 || f.status == 0)) page_viewer.display_markdown(f.responseText) 
+        }
+        f.send(null)
+    }
+}
+const page_changer = {
+    pages_length: null,
+    current_page: null,
+    move_slide: (offset) => {
+        if (offset < 0 && page_changer.current_page <= 0) return
+        else if (offset > 0 && page_changer.current_page >= page_changer.pages_length -1) return
+        
+        page_changer.change_slide(page_changer.current_page + offset)
+    },
+    change_slide: (page_number) => {
+        const pages = Array.from(document.querySelectorAll('[data-page]'))
+        
+        page_changer.pages_length = pages.length
+        
+        pages.forEach((page,index) => {
+            page.style.display = 'none'
+            if (page_number == index) {
+                page.style.display = 'block'
+                page_changer.current_page = index
+
+                page_info.innerText = `${page_number + 1} of ${pages.length}`
+
+                back_btn.disabled = false
+                forward_btn.disabled = false
+                document.body.scrollTop = document.documentElement.scrollTop = 0;
+            }
+            if (page_number == page_changer.pages_length-1) forward_btn.disabled = true 
+            else if (page_number == 0) back_btn.disabled = true 
+        })
+    },
+    change_to_md: (file_location) => {
+        menu_element.style.display = 'none'
+        page_element.style.display = 'block'
+
+        page_viewer.read_file(file_location)
+        page_changer.change_slide(0)
+    },
+    change_to_menu: () => {
+        menu_element.style.display = 'block'
+        page_element.style.display = 'none'
+    },
+}
+const menu_loader = {
+    prefix: './md_files/',
+    menu: [],
+    import: (title, desc, src) => {menu_loader.menu.push({title, desc, src})},
+    load: () => {
+        menu_element.innerHTML = menu_loader.menu.map((item, index) => `
+            <div class="item" onclick="page_changer.change_to_md('${menu_loader.prefix + item.src}')">
+                <div class="numbered">${index+1}</div>
+                <div class="text">
+                    <div class="left">
+                        <h1>${item.title}</h1>
+                        <p>${item.desc}</p>
+                    </div>
+                    <div class="right">
+                        <button onclick="page_changer.change_to_md('${menu_loader.prefix + item.src}')"><i class="material-icons">start</i></button>
+                    </div>
+                </div>
+            </div>
+        `).join('')
+    }
+}
+
+// import pages
+menu_loader.import(
+    title='Lesson 1', 
+    desc='What is HTML? You will learn this as well as making your first ever website!', 
+    src='1.what.is.html.md'
+)
+menu_loader.import(
+    title='Poem Activity', 
+    desc="Practice the skills you've learnt from Lesson 1 by making a website that displays a poem.", 
+    src='hello.md'
+)
+// load the menu
+menu_loader.load()
+page_changer.change_to_menu()
---
+++ help/md_files/hello.md
@@ -0,0 +1,30 @@
+@@page-start
+
+# Welcome to StackEdit!
+
+lorem
+
+#code-start#
+const hello = why/n
+alert('what is your name')
+alert('end')
+#code-end#
+
+!https://images.unsplash.com/photo-1661347333238-0c8fe353e59e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80
+
+## Switch to another file
+
+All your files and folders are presented as a tree in the file explorer. You can switch from one to another by clicking a file in the tree.
+
+@@page-end
+@@page-start
+
+## Rename a file
+
+You can rename the current file by clicking the file name in the navigation bar or by clicking the **Rename** button in the file explorer.
+
+## Delete a file
+
+You can delete the current file by clicking the **Remove** button in the file explorer. The file will be moved into the **Trash** folder and automatically deleted after 7 days of inactivity.
+
+@@page-end
